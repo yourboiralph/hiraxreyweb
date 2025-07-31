3937364e9d7d83cf3b83294087bcc3069e7258aa
ralph hernandez <mac@ralphs-MacBook-Air.local>
2025-07-31 10:22:08 +0800

    added background

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,7 +5,7 @@ import Hero from '../components/Hero'
 
 const Home = () => {
   return (
-    <div className='min-h-screen bg-background text-foreground overflow-x-hidden'>
+    <div className='min-h-screen bg-gradient-to-br from-orange-400 to-blue-200 text-foreground overflow-x-hidden'>
       <Navbar />
       <main>
         <Hero />
